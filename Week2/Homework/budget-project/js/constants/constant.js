b74807a77044bf9a98833bf12ed6c3d8dc05e917
kimminna <kmina121777@naver.com>
2026-04-16 21:32:37 +0900

    chore: 프로젝트 전반에서 쓰이는 가계부 관련 상수 파일 추출

---
+++ Week2/Homework/budget-project/js/constants/constant.js
@@ -0,0 +1,21 @@
+/** 로컬스토리지 키 값 관리 목록 */
+export const STORAGE_KEY = "accountData";
+
+/** 카테고리 목록 */
+export const CATEGORIES = ["식비", "교통", "월급", "생활", "문화", "기타"];
+
+/** 결제수단 목록 */
+export const PAYMENT_METHODS = ["현금", "신용카드", "체크카드"];
+
+/** 얼럿 메시지 관리 */
+export const MESSAGES = {
+  EMPTY_INPUT: "입력값을 확인해주세요.",
+  CONFIRM_DELETE: "정말 삭제하시겠습니까?",
+  NO_SELECTION: "삭제할 항목을 선택해주세요.",
+};
+
+/** 가계부 타입 목록 */
+export const TRANSACTION_TYPE = {
+  INCOME: "수입",
+  EXPENSE: "지출",
+};
