9796d0f151d993b471055ef4b96e86d23092b7a0
kimminna <kmina121777@naver.com>
2026-04-25 16:23:53 +0900

    feat: 검색 기능 도입을 위한 검색바 컴포넌트 및 useSearch 커스텀 훅 파일 생성

--- Week3/Practice/my-app/src/App.jsx
+++ Week3/Practice/my-app/src/App.jsx
@@ -1,9 +1,26 @@
-function App() {
+import { Card } from "./components/Card";
+import { Header } from "./components/Header";
+import { Search } from "./components/Search";
+import { members } from "./mocks/member";
+import { useSearch } from "./hooks/useSearch";
+
+export default function App() {
+  const { search, filteredData, handleSearchChange, handleSearchClick } =
+    useSearch(members);
+
   return (
-    <>
-      <h1>반가워요</h1>
-    </>
+    <section>
+      <Header />
+      <Search
+        search={search}
+        onSearchChange={handleSearchChange}
+        onSearchClick={handleSearchClick}
+      />
+      <section style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+        {filteredData.map((member) => (
+          <Card key={member.id} {...member} />
+        ))}
+      </section>
+    </section>
   );
 }
-
-export default App;
