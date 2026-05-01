b3b89a69da577a20dda83f449a224235d53f07fe
kimminna <kmina121777@naver.com>
2026-05-01 19:32:42 +0900

    style: 카드 컴포넌트 배경 색 변경

--- Week3/Homework/src/components/Card.tsx
+++ Week3/Homework/src/components/Card.tsx
@@ -5,7 +5,7 @@ interface CardProps {
 
 export const Card = ({label, message}: CardProps) => {
   return (
-    <div className='bg-primary-200 flex flex-col items-center justify-between rounded-2xl p-2'>
+    <div className='bg-primary-100 flex flex-col items-center justify-between rounded-2xl p-2'>
       <h2 className='text-h2'>{label}</h2>
       <span className='text-h1'>{message}</span>
     </div>
