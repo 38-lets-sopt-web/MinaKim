ac4575fb4f347caf9ebc195cce5e0dba5fafdb6d
kimminna <kmina121777@naver.com>
2026-05-07 15:45:20 +0900

    feat: 쿼리 provider 적용 및 스타일 파일 임포트

--- Week4/Homework/src/App.tsx
+++ Week4/Homework/src/App.tsx
@@ -1,7 +1,13 @@
 import {router} from '@/app/providers/router';
-import '@/shared/styles/index.css';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {RouterProvider} from 'react-router';
 
+const queryClient = new QueryClient();
+
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
--- Week4/Homework/src/main.tsx
+++ Week4/Homework/src/main.tsx
@@ -1,3 +1,4 @@
+import '@/shared/styles/index.css';
 import App from '@/App';
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
