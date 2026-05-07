520ff4c931f411de8b74bc210b585bc21ac3d0ec
kimminna <kmina121777@naver.com>
2026-05-07 15:00:32 +0900

    feat: 마이페이지 레이아웃 전용 헤더에 유저 이름 전달 및 Outlet 추가

--- Week4/Homework/src/pages/my-page-layout/ui/Page.tsx
+++ Week4/Homework/src/pages/my-page-layout/ui/Page.tsx
@@ -1,9 +1,22 @@
+import {useUserQuery} from '@/features/user/model/useUserQuery';
 import {Header} from '@/widgets/header';
+import {Navigate, Outlet} from 'react-router';
 
 export default function MyPageLayout() {
+  const userId = Number(localStorage.getItem('userId'));
+
+  const {data: user} = useUserQuery(userId);
+
+  if (!userId) {
+    return <Navigate to='/login' replace />;
+  }
+
   return (
     <div>
-      <Header />
+      <Header userName={user?.name ?? ''} />
+      <main className='flex flex-col items-center justify-center'>
+        <Outlet />
+      </main>
     </div>
   );
 }
