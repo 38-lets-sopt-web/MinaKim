7d1106a9d50313d41c826ed1c83e60be2e327ff9
kimminna <kmina121777@naver.com>
2026-05-26 00:23:23 +0900

    feat: 기본 네트워크 통신 세팅 - http 요청 커스텀 - axios 인스턴스 설정 - 쿼리 클라이언트 추가

--- Week6/Homework/src/main.tsx
+++ Week6/Homework/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router-dom";
+import router from "./shared/lib/router/router";
 import "./index.css";
-import App from "./App.tsx";
+import { queryClient } from "@/shared/lib/react-query/queryClient";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
