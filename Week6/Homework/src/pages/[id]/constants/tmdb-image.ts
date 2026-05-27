014ba949fef85b464618c753ce55a2da7b578583
kimminna <kmina121777@naver.com>
2026-05-28 00:34:03 +0900

    chore: 이미지 베이스 상수 추출

---
+++ Week6/Homework/src/pages/[id]/constants/tmdb-image.ts
@@ -0,0 +1,4 @@
+export const TMDB_IMAGE_BASE_W500 = "https://image.tmdb.org/t/p/w500";
+export const TMDB_IMAGE_BASE_ORIGINAL = "https://image.tmdb.org/t/p/original";
+export const FALLBACK_POSTER = "https://placehold.co/500x750?text=No+Image";
+export const FALLBACK_BACKDROP = "https://placehold.co/1280x720?text=No+Image";
